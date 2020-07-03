1f770daaa42c8dad914fa4a462c890b7e8f42122
mahmutcankurt <is.mahmutcankurt@gmail.com>
2020-07-03 21:40:39 +0300

    asdfgh

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,139 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
+import 'package:location/location.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+import 'package:uygulama1/Weather.dart';
+import 'package:uygulama1/WeatherItem.dart';
+import 'package:uygulama1/WeatherData.dart';
+import 'package:uygulama1/ForecastData.dart';
 
-class MyApp extends StatelessWidget {
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Weather App',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
+  State<StatefulWidget> createState() {
+    return new MyAppState();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  var _locality = '';
-  var _weather = '';
-  
-
-  Future<Position> getPosition() async {
-    Position position = await Geolocator()
-      .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    return position;
-  }
-
-  Future<Placemark> getPlacemark(double latitude, double longitude) async {
-    List<Placemark> placemark = await Geolocator()
-      .placemarkFromCoordinates(latitude, longitude);
-    return placemark[0];
-  }
-
-  Future<String> getData(double latitude, double longitude) async {
-    String api = 'http://api.openweathermap.org/data/2.5/forecast';
-    String appId = 'd89de3f0b2dedfe4f923f1e7f709953a';
-    
-    String url = '$api?lat=$latitude&lon=$longitude&APPID=$appId';
-    
-    http.Response response = await http.get(url);
-
-    Map parsed = json.decode(response.body);
-
-    print(parsed);
-    
-    return (parsed['list'][0]['main']['temp']).toString();
-  }
+class MyAppState extends State<MyApp> {
+  bool isLoading = false;
+  WeatherData weatherData;
+  ForecastData forecastData;
+  Location _location = new Location();
+  String error;
 
   @override
   void initState() {
     super.initState();
-    getPosition().then((position) {
-      getPlacemark(position.latitude, position.longitude).then((data) {
-        getData(position.latitude, position.longitude).then((weather) {
-          setState(() {
-            _locality = data.locality;
-            _weather = weather;
-          });
-        });
-      });
-    });
+
+    loadWeather();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Weather App'),
+    return MaterialApp(
+      title: 'Flutter Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$_locality',
-            ),
-            Text(
-              '$_weather',
-            ),
-          ],
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('Flutter Weather App'),
         ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: weatherData != null ? Weather(weather: weatherData) : Container(),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: isLoading ? CircularProgressIndicator(
+                        strokeWidth: 2.0,
+                        valueColor: new AlwaysStoppedAnimation(Colors.white),
+                      ) : IconButton(
+                        icon: new Icon(Icons.refresh),
+                        tooltip: 'Refresh',
+                        onPressed: loadWeather,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 200.0,
+                    child: forecastData != null ? ListView.builder(
+                        itemCount: forecastData.list.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) => WeatherItem(weather: forecastData.list.elementAt(index))
+                    ) : Container(),
+                  ),
+                ),
+              )
+            ]
+          )
+        )
       ),
     );
+  }
+
+  loadWeather() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    Map<String, double> location;
+    try {
+      location = await _location.getLocation;
+
+      error = null;
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED') {
+        error = 'Permission denied';
+      } else if (e.code == 'PERMISSION_DENIED_NEVER_ASK') {
+        error = 'Permission denied - please ask the user to enable it from the app settings';
+      }
+
+      location = null;
+    }
+
+    if (location != null) {
+      final lat = location['latitude'];
+      final lon = location['longitude'];
+
+      final weatherResponse = await http.get(
+          'https://api.openweathermap.org/data/2.5/weather?APPID=0721392c0ba0af8c410aa9394defa29e&lat=${lat.toString()}&lon=${lon.toString()}');
+      final forecastResponse = await http.get(
+          'https://api.openweathermap.org/data/2.5/forecast?APPID=0721392c0ba0af8c410aa9394defa29e&lat=${lat.toString()}&lon=${lon.toString()}');
+
+      if (weatherResponse.statusCode == 200 &&
+          forecastResponse.statusCode == 200) {
+        return setState(() {
+          weatherData =
+          new WeatherData.fromJson(jsonDecode(weatherResponse.body));
+          forecastData =
+          new ForecastData.fromJson(jsonDecode(forecastResponse.body));
+          isLoading = false;
+        });
+      }
+    }
+
+    setState(() {
+      isLoading = false;
+    });
   }
 }
